eac8d3710d9baa3981ee744e691f0de1d96f0d76
codethefeel <yogeshsolankim76@gmail.com>
2024-03-07 18:58:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        blue: "#2870EA",
+        midnight: "#121063",
+      },
+    },
+  },
+  plugins: [],
+};
